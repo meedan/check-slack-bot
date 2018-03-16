7ce6c8277a074830d3924b94e286641542954cf1
Caio Almeida <caiosba@gmail.com>
2018-03-16 19:06:37 -0300

    Ticket #6756: Adding one more test

--- buttons.test.js
+++ buttons.test.js
@@ -14,6 +14,22 @@ const buildData = (token, type, payload) => {
   return data;
 };
 
+const buildPayload = (teamId) => {
+  const payload = {
+    team: {
+      id: teamId
+    },
+    original_message: {
+      attachments: [
+        {
+          title_link: 'Test',
+        },
+      ],
+    },
+  };
+  return payload;
+};
+
 test('verify call if team is in config', () => {
   const data = buildData('123456abcdef', 'url_verification');
   const callback = jest.fn();
@@ -26,4 +42,12 @@ test('does not verify call if team is not in config', () => {
   const callback = jest.fn();
   buttons.handler(data, null, callback);
   expect(callback).toHaveBeenCalledWith('Verification failed');
+});
+
+test('does not process call if verification token is not valid', () => {
+  const payload = buildPayload('T12345ABC');
+  const data = buildData('invalid', 'process', payload);
+  const callback = jest.fn();
+  buttons.handler(data, null, callback);
+  expect(callback).toHaveBeenCalledWith(null, expect.objectContaining({ text: expect.stringContaining('do not have the permission') }))
 });
